59827283fe0b582f8862c4acf2b67c91a99a3f32
Santanu Kumar Sahu <santanu.sahu@nutanix.com>
2025-01-02 12:38:55 +0530

    refactor: migrate server setup to a new file and update start script

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', '*')
     next()
 });
